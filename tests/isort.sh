d8cfa6e50b7316628bcb6fb2fd426be68538c1f7
Thomas Defise <t.defise@gmail.com>
2021-02-19 16:45:20 +0100

    Reviewed SecretScan

--- tests/isort.sh
+++ tests/isort.sh
@@ -3,7 +3,9 @@
 isort --diff --profile black .. > result.txt
 cat result.txt
 
-if [ $(wc -l result.txt) -gt 1 ];then
+$numberLine=$(wc -l result.txt)
+
+if [ $numberLine -gt 1 ];then
   echo "Imports are not sorted alphabetically"
   exit 1
 fi
