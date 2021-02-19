86b97d4541f8a7d4bb30e82110358a7ee829bc27
Thomas Defise <t.defise@gmail.com>
2021-02-19 18:14:32 +0100

    modified isort

--- tests/isort.sh
+++ tests/isort.sh
@@ -4,10 +4,11 @@ isort --diff --profile black .. > result.txt
 cat result.txt
 
 numberLine=$(wc -l result.txt)
-
+echo $numberLine
 if [ ${numberLine} -gt 1 ];then
   echo "Imports are not sorted alphabetically"
   exit 1
 fi
 
+echo "Imports are sorted alphabetically"
 exit 0
